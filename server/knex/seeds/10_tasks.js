54e2bac6ef2253992840818caf9b0eed8fd18fbd
Evan M. Bonk <embonk@hotmail.com>
2025-09-08 12:44:11 -0600

    seed data task and history

--- server/knex/seeds/10_tasks.js
+++ server/knex/seeds/10_tasks.js
@@ -1,13 +1,11 @@
 /**
  * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
+ * @returns { Promise<void> }
  */
-exports.seed = async function(knex) {
+exports.seed = async function (knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
+  await knex('tasks').del()
+  await knex('tasks').insert([
+    { title: 'rowValue1', description: 'some description', mission_id: 1, status: 1, due_date: '2025-09-30', assignee: 1 },
   ]);
 };
--- server/knex/seeds/11_history.js
+++ server/knex/seeds/11_history.js
@@ -1,13 +1,11 @@
 /**
  * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
+ * @returns { Promise<void> }
  */
-exports.seed = async function(knex) {
+exports.seed = async function (knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
+  await knex('history').del()
+  await knex('history').insert([
+    { task: 1, operation: 1, user: 1 },
   ]);
 };
