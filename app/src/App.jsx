02d859cc25ba47fb7efe45046e7e819c532ec04e
stako5 <bankhead089@gmail.com>
2025-09-08 23:09:07 +0200

    Updating the NavBar

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,3 +1,7 @@
+import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
+
+import AppContext from "./AppContext";
 import NavBar from "./component/NavBar";
 import SideBar from "./component/SideBar";
 import Footer from "./component/Footer";
@@ -5,12 +9,21 @@ import Homepage from "./pages/Homepage";
 import "../css/App.css";
 
 export default function App() {
+  const [user, setUser] = useState(null);
+
   return (
-    <>
+    <AppContext.Provider value={{ user: user, setUser: setUser }}>
       <NavBar />
       <SideBar />
-      <Homepage />
       <Footer />
-    </>
+
+      <Routes>
+        <Route />
+        <Route />
+        <Route />
+        <Route />
+        <Route />
+      </Routes>
+    </AppContext.Provider>
   );
 }
--- app/src/component/NavBar.jsx
+++ app/src/component/NavBar.jsx
@@ -1,7 +1,26 @@
+import { useContext } from "react";
+import { Link } from "react-router";
+
+import AppContext from "../AppContext";
+
 export default function NavBar() {
+  const { user } = useContext(AppContext);
+
   return (
     <nav>
-      <h1>text</h1>
+      <img src="src/assets/Nova-icon.png" />
+      <h1>NovaStack</h1>
+      {user ? (
+        <>
+          <p>🔔</p>
+          <Link>{user}</Link>
+          <Link>Logout</Link>
+        </>
+      ) : (
+        <>
+          <Link>login</Link>
+        </>
+      )}
     </nav>
   );
 }
