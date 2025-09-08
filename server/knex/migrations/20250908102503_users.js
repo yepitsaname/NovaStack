5609d7576c1ac712aed8f9ee9a898e030f8b2548
Evan M. Bonk <embonk@hotmail.com>
2025-09-08 10:46:41 -0600

    user migration

--- server/knex/migrations/20250908102503_users.js
+++ server/knex/migrations/20250908102503_users.js
@@ -1,9 +1,16 @@
 'use strict';
 
-exports.up = function(knex, Promise) {
-  
+exports.up = function (knex, Promise) {
+  return knex.schema.createTable('users', table => {
+    table.increments('user_id');
+    table.string('username');
+    table.string('email');
+    table.text('password');
+    table.string('first_name');
+    table.string('last_name');
+  })
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function (knex, Promise) {
+  return knex.schema.dropTableIfExists('users');
 };
