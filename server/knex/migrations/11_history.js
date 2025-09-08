125cfa897dbddf9d0271c2bf5e218fd7a1869f55
Evan M. Bonk <embonk@hotmail.com>
2025-09-08 12:33:03 -0600

    added seed files

--- server/knex/migrations/11_history.js
+++ server/knex/migrations/11_history.js
@@ -3,8 +3,11 @@
 exports.up = function (knex, Promise) {
   return knex.schema.createTable('history', table => {
     table.increments('history_id');
+    table.integer('task');
     table.foreign('task').references('tasks.task_id');
+    table.integer('operation');
     table.foreign('operation').references('operations.operation_id');
+    table.integer('user');
     table.foreign('user').references('users.user_id');
     table.dateTime('date_time');
   });
