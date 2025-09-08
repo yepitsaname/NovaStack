e55caf178643b43af13268258add692a9d85e95e
Gavin Parra <gavinparra@ymail.com>
2025-09-08 12:47:55 -0600

    seeds 4-6

--- server/knex/seeds/04_audit_action.js
+++ server/knex/seeds/04_audit_action.js
@@ -4,10 +4,8 @@
  */
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
+  await knex('audit_action').del()
+  await knex('audit_action').insert([
+    {action_name: 'exampleAction'},
   ]);
 };
