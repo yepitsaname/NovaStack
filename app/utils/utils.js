052ac2c40f4d0e93a24dbd63b72266de3961247b
Gavin Parra <gavinparra@ymail.com>
2025-09-08 14:20:03 -0600

    beginning utils

---
+++ app/utils/utils.js
@@ -0,0 +1,34 @@
+const backend = "http://localhost:3000";
+
+//template
+export function Example() {
+  return fetch(`${backend}`).then((res) => res.json());
+};
+
+export function GetUser(id) {
+  return fetch(`${backend}/user/${id}`).then((res) => res.json());
+};
+
+export function GetAllTasks() {
+  return fetch(`${backend}/tasks`).then((res) => res.json());
+};
+
+export function GetTaskById(id) {
+  return fetch(`${backend}/tasks/${id}`).then((res) => res.json());
+};
+
+export function GetTaskByStatus(id) {
+  return fetch(`${backend}/status/${id}`).then((res) => res.json());
+};
+
+export function GetAllMissions() {
+  return fetch(`${backend}/missions`).then((res) => res.json());
+};
+
+export function GetTaskByMission(id) {
+  return fetch(`${backend}/mission/${id}/tasks`).then((res) => res.json());
+};
+
+export function GetSystemStatus(id) {
+  return fetch(`${backend}/mission/${id}/systems`).then((res) => res.json());
+};
