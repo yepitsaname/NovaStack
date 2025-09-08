b2b33bb99d907c3801dd37b87ea3b099dd87a21e
bwgnull <jacob.kosswig@gmail.com>
2025-09-08 11:08:26 -0600

    styling2

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -4,6 +4,18 @@ function App() {
 
   return (
     <>
+      <div className='component'>
+        <div className='header'>
+          <h1>Rofl</h1>
+          <h2>Sdiybt</h2>
+          <h3>Yolo</h3>
+          <h4>Gurt</h4>
+          <h5>Blevin</h5>
+          <h6>Vro</h6>
+        </div>
+        <button>button</button>
+        <a>myLink</a>
+      </div>
     </>
   )
 }
