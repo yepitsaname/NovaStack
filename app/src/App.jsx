12f56f759fb41893e5460955dc394f8d83099587
stako5 <bankhead089@gmail.com>
2025-09-08 22:11:23 +0200

    Update main page styling

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,28 +1,16 @@
-function App() {
+import NavBar from "./component/NavBar";
+import SideBar from "./component/SideBar";
+import Footer from "./component/Footer";
+import Homepage from "./pages/Homepage";
+import "../css/App.css";
 
+export default function App() {
   return (
     <>
-      <div className='component'>
-        <div className='header'>
-          <h1>Rofl</h1>
-          <h2>Sdiybt</h2>
-          <h3>Yolo</h3>
-          <h4>Gurt</h4>
-          <h5>Blevin</h5>
-          <h6>Vro</h6>
-        </div>
-        <button>button</button>
-        <a>myLink</a>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet magnam maxime ut facere qui atque rerum porro id aliquid provident, animi illo ab accusamus aut ea deserunt temporibus. Vitae, labore!</p>
-        <form className="form component">
-          <textarea placeholder="Hello World" />
-          <input type= 'text' placeholder="Place text here"/>
-          <button>button</button>
-          <a>myLink</a>
-        </form>
-      </div>
+      <NavBar />
+      <SideBar />
+      <Homepage />
+      <Footer />
     </>
-  )
+  );
 }
-
-export default App
---
+++ app/src/component/SideBar.jsx
@@ -0,0 +1,7 @@
+export default function SideBar() {
+  return (
+    <aside>
+      <h1>text</h1>
+    </aside>
+  );
+}
