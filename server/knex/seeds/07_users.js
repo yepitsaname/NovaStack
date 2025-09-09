a95db050d2e14fd0711ef64bf7cfb1214a2350f0
Evan M. Bonk <embonk@hotmail.com>
2025-09-09 14:19:37 -0600

    Encrypted legoman's password

--- server/knex/seeds/07_users.js
+++ server/knex/seeds/07_users.js
@@ -1,11 +1,11 @@
 /**
  * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
+ * @returns { Promise<void> }
  */
-exports.seed = async function(knex) {
+exports.seed = async function (knex) {
   // Deletes ALL existing entries
   await knex('users').del()
   await knex('users').insert([
-    {username: 'legoman', email: 'lego@gmail.com', password: 'password', first_name: 'Noah', last_name: 'Lego'}
+    { username: 'legoman', email: 'lego@gmail.com', password: '$2a$10$7PkdPzBA/mLIJS.9GgMJx.Pmf6R2Xjt5gaZ3LwkMC/hxR80gnh592', first_name: 'Noah', last_name: 'Lego' }
   ]);
 };
