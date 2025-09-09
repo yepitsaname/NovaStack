8226260f67fe3db3e644638ad4c91ab114829006
bwgnull <jacob.kosswig@gmail.com>
2025-09-09 09:20:47 -0600

    sidebar-navbar-styling

--- app/src/component/Footer.jsx
+++ app/src/component/Footer.jsx
@@ -1,7 +1,12 @@
 export default function Footer() {
+  setInterval(() => {
+    const date = new Date()
+    document.querySelector(".date-time").innerHTML = date.toUTCString()
+  }, 1000)
+
   return (
     <footer>
-      <p>text</p>
+      <p className = 'date-time'></p>
     </footer>
   );
 }
--- app/src/component/SideBar.jsx
+++ app/src/component/SideBar.jsx
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom"
+
+
 export default function SideBar() {
   return (
     <aside>
-      <h1>text</h1>
+      <Link>WWWWWWWWW</Link>
+      <Link>WWWWWWWWW</Link>
+      <Link>WWWWWWWWW</Link>
+      <Link>WWWWWWWWW</Link>
+      <Link>WWWWWWWWW</Link>
+      <Link>WWWWWWWWW</Link>
     </aside>
   );
 }
