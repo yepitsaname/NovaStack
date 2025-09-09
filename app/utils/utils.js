82a0af0ad86f41438e84cf02cf2845a4755aa33c
Evan M. Bonk <embonk@hotmail.com>
2025-09-09 12:52:40 -0600

    front-end util functions updated to require tokens

--- app/utils/utils.js
+++ app/utils/utils.js
@@ -1,37 +1,88 @@
 const backend = "http://localhost:3000";
 
-export function GetUser(id) {
-  return fetch(`${backend}/user/${id}`).then((res) => res.json());
+export async function GetUser(id, token) {
+  return fetch(`${backend}/user/${id}`, {
+    method: "GET",
+    headers: { Authorization: token }
+  }).then((res) => res.json());
 };
 
-export function GetAllTasks() {
-  return fetch(`${backend}/tasks`).then((res) => res.json());
+export async function UserLogin(username, password) {
+  return fetch(`${backend}/login`, {
+    method: "POST",
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify({
+      userName: username,
+      password: password
+    })
+  })
+    .then((res) => res.json());
+}
+
+export async function UserSignup(userName, password, firstName, lastName, email) {
+  return fetch(`${backend}/signup`, {
+    method: "POST",
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify({
+      userName: userName,
+      password: password,
+      firstName: firstName,
+      lastName: lastName,
+      email: email
+    })
+  })
+    .then((res) => res.json());
+}
+
+export async function GetAllTasks(token) {
+  return fetch(`${backend}/tasks`, {
+    method: "GET",
+    headers: { Authorization: token }
+  }).then((res) => res.json());
 };
 
-export function GetTaskById(id) {
-  return fetch(`${backend}/tasks/${id}`).then((res) => res.json());
+export async function GetTaskById(id, token) {
+  return fetch(`${backend}/tasks/${id}`, {
+    method: "GET",
+    headers: { Authorization: token }
+  }).then((res) => res.json());
 };
 
-export function GetTaskByStatus(id) {
-  return fetch(`${backend}/status/${id}`).then((res) => res.json());
+export async function GetTaskByStatus(id, token) {
+  return fetch(`${backend}/status/${id}`, {
+    method: "GET",
+    headers: { Authorization: token }
+  }).then((res) => res.json());
 };
 
-export function GetAllMissions() {
-  return fetch(`${backend}/missions`).then((res) => res.json());
+export async function GetAllMissions(token) {
+  return fetch(`${backend}/missions`, {
+    method: "GET",
+    headers: { Authorization: token }
+  }).then((res) => res.json());
 };
 
-export function GetTaskByMission(id) {
-  return fetch(`${backend}/mission/${id}/tasks`).then((res) => res.json());
+export async function GetTaskByMission(id, token) {
+  return fetch(`${backend}/mission/${id}/tasks`, {
+    method: "GET",
+    headers: { Authorization: token }
+  }).then((res) => res.json());
 };
 
 // export function GetSystemStatus(id) {
 //   return fetch(`${backend}/mission/${id}/systems`).then((res) => res.json());
 // };
 
-export function GetAllHistory() {
-  return fetch(`${backend}/history`).then((res) => res.json());
+export function GetAllHistory(token) {
+  return fetch(`${backend}/history`, {
+    method: "GET",
+    headers: { Authorization: token }
+  }).then((res) => res.json());
 };
 
-export function GetUsersByRole(id) {
-  return fetch(`${backend}/roles/${id}`).then((res) => res.json());
+export function GetUsersByRole(id, token) {
+  return fetch(`${backend}/roles/${id}`, {
+    method: "GET",
+    headers: { Authorization: token }
+  }).then((res) => res.json());
 };
