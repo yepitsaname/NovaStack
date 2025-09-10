48559823a2a6272334eeab4aaad6af2b3b323413
Gavin Parra <gavinparra@ymail.com>
2025-09-10 09:05:10 -0600

    added titles

--- app/src/pages/Dashboard.jsx
+++ app/src/pages/Dashboard.jsx
@@ -2,12 +2,15 @@ export default function Dashboard() {
   return (
     <>
       <div>
+        <h1>Mission Control Dashboard</h1>
         <p className="date-time">
+          
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
           aspernatur laudantium soluta nobis. In minima, adipisci velit vero
           illo dolor nobis pariatur sequi maiores provident repellat obcaecati
           laudantium? Accusamus, eligendi?
         </p>
+        
       </div>
     </>
   );
