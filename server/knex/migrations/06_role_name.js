8daedb135715bf46b41ef767d1e2c50d87c21329
Evan M. Bonk <embonk@hotmail.com>
2025-09-10 15:01:05 -0600

    altered role table

--- server/knex/migrations/06_role_name.js
+++ server/knex/migrations/06_role_name.js
@@ -4,7 +4,8 @@ exports.up = function (knex, Promise) {
   return knex.schema.createTable('role_name', table => {
     table.increments('role_name_id');
     table.string('role_name');
-    table.string("layout")
+    table.string("layout");
+    table.jsonb("default_layout").defaultTo("{}")
   });
 };
 
