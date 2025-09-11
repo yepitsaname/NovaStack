707f9054820e4a7d43a2e86cf9d49139def7165c
Gavin Parra <gavinparra@ymail.com>
2025-09-11 14:09:27 -0600

    force user back to login page at logout, and disallows use of sidepanel

--- app/src/component/SideBar.jsx
+++ app/src/component/SideBar.jsx
@@ -1,13 +1,16 @@
 import { Link } from "react-router-dom";
-
+import AppContext from "../AppContext";
+import { useContext } from "react";
 export default function SideBar() {
+const { token } = useContext(AppContext);
+
   return (
     <aside>
-      <Link to="/dashboard">Dashboard</Link>
-      <Link to="/system">System</Link>
-      <Link to="/taskslist">Tasks</Link>
-      <Link to="/reports">Reports</Link>
-      <Link to="/configuration">Configuration</Link>
+      <Link to={!token ? "/login" : "/Dashboard"}>Dashboard</Link>
+      <Link to={!token ? "/login" : "/System"}>System</Link>
+      <Link to={!token ? "/login" : "/Tasks"}>Tasks</Link>
+      <Link to={!token ? "/login" : "/Reports"}>Reports</Link>
+      <Link to={!token ? "/login" : "/Configuration"}>Configuration</Link>
     </aside>
   );
 }
