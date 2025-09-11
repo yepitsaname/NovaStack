f86202c5eadcba13f1565fb2835867fba6d3e933
Gavin Parra <gavinparra@ymail.com>
2025-09-11 14:43:45 -0600

    edited some login stuff and styling for logout

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -36,9 +36,9 @@ export default function App() {
       }}
     >
       <NavBar />
-      <SideBar />
       <Footer />
-      <div className="main-content">
+      <main className="main-content">
+      <SideBar />
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Signup />} />
@@ -53,7 +53,7 @@ export default function App() {
           <Route path="/taskslist/edit" element={<EditTask />} />
           <Route path="/taskslist/:id" element={<TaskItem />} />
         </Routes>
-      </div>
+      </main>
     </AppContext.Provider>
   );
 }
--- app/src/component/SideBar.jsx
+++ app/src/component/SideBar.jsx
@@ -1,16 +1,19 @@
 import { Link } from "react-router-dom";
 import AppContext from "../AppContext";
 import { useContext } from "react";
+
 export default function SideBar() {
-const { token } = useContext(AppContext);
+const { token, user, profile } = useContext(AppContext);
 
   return (
-    <aside>
-      <Link to={!token ? "/login" : "/Dashboard"}>Dashboard</Link>
-      <Link to={!token ? "/login" : "/System"}>System</Link>
-      <Link to={!token ? "/login" : "/Tasks"}>Tasks</Link>
-      <Link to={!token ? "/login" : "/Reports"}>Reports</Link>
-      <Link to={!token ? "/login" : "/Configuration"}>Configuration</Link>
-    </aside>
+    <>
+      {!token||!user||!profile ? <></> :( <aside>
+        <Link to={!token||!user||!profile ? "/login" : "/Dashboard"}>Dashboard</Link>
+        <Link to={!token||!user||!profile ? "/login" : "/System"}>System</Link>
+        <Link to={!token||!user||!profile ? "/login" : "/Tasks"}>Tasks</Link>
+        <Link to={!token||!user||!profile ? "/login" : "/Reports"}>Reports</Link>
+        <Link to={!token||!user||!profile ? "/login" : "/Configuration"}>Configuration</Link>
+      </aside> )}
+  </>
   );
 }
