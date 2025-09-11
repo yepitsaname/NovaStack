419f1b5c1b5febacc621c3a743d1ebbb1c4a5984
yepitsaname <fernando.g.berry@gmail.com>
2025-09-11 17:05:50 -0400

    profile now changes "theme" for users

--- app/src/pages/Profile.jsx
+++ app/src/pages/Profile.jsx
@@ -1,53 +1,58 @@
 import { useEffect, useContext } from "react";
 import AppContext from "../AppContext";
-import { GetUser } from "../../utils/utils";
 
 export default function Profile() {
-  const { user, profile } = useContext(AppContext);
+  const { user, profile, setProfile } = useContext(AppContext);
+  let theme = profile.preferences.theme;
 
   // Edit actions
   // should update the user's prefered scheme in the
   // database and on their profile
-  const updateTheme = (event) => {}
+  const updateTheme = () => {
+    setProfile((current)=>{
+      return Object.assign(current,
+        {preferences: {theme: theme}}
+      )})
+  }
 
 
   if( !profile ) return <div className="form"><h2>Loading Profile</h2></div>
 
   return (
     <div className="form">
-        <h2>{user}'s Profile</h2>
-        <fieldset name="user information">
-          <legend>Overview</legend>
-          <label htmlFor="username">Username</label>
-          <input type="text" id="username" name="user name" value={user} disabled/>
-          <label htmlFor="firstname">First Name</label>
-          <input type="text" id="firstname" name="first name" value={profile.first_name} disabled/>
-          <label htmlFor="lastname">Last Name</label>
-          <input type="text" id="lastname" name="last name" value={profile.last_name} disabled/>
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" name="email" value={profile.email} disabled />
-          <label htmlFor="roles">Roles</label>
-          <input type="text" id="roles" name="roles" value={profile.roles.map(role=>role.role_name).join(", ")} disabled />
-        </fieldset>
-        <fieldset>
-          <legend>Theming</legend>
-          <label htmlFor="theme">Themes</label>
-          <select defaultValue={profile.theme}>
-            <option value="dark">Dark</option>
-            <option value="light">Light</option>
-          </select>
-          <button onClick={()=>{}}>Update</button>
-        </fieldset>
-        <fieldset>
-          <legend>Password Reset</legend>
-          <label htmlFor="current-password">Current Password</label>
-          <input type="password" id="current-password" name="current password" />
-          <label htmlFor="new-password">New Password</label>
-          <input type="password" id="new-password" name="new password" />
-          <label htmlFor="confirm-password">Confirm Password</label>
-          <input type="password" id="confirm-password" name="confirm password" />
-          <button>Reset Password</button>
-        </fieldset>
+      <h2>{user}'s Profile</h2>
+      <fieldset name="user information">
+        <legend>Overview</legend>
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" name="user name" value={user} disabled/>
+        <label htmlFor="firstname">First Name</label>
+        <input type="text" id="firstname" name="first name" value={profile.first_name} disabled/>
+        <label htmlFor="lastname">Last Name</label>
+        <input type="text" id="lastname" name="last name" value={profile.last_name} disabled/>
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" name="email" value={profile.email} disabled />
+        <label htmlFor="roles">Roles</label>
+        <input type="text" id="roles" name="roles" value={profile.roles.map(role=>role.role_name).join(", ")} disabled />
+      </fieldset>
+      <fieldset>
+        <legend>Theming</legend>
+        <label htmlFor="theme">Themes</label>
+        <select defaultValue={profile.theme} onChange={(event)=>{theme = event.target.value}}>
+          <option value="dark">Dark</option>
+          <option value="light">Light</option>
+        </select>
+        <button onClick={()=>{updateTheme()}}>Update</button>
+      </fieldset>
+      <fieldset>
+        <legend>Password Reset</legend>
+        <label htmlFor="current_password">Current Password</label>
+        <input type="password" id="current_password" name="current password" />
+        <label htmlFor="new_password">New Password</label>
+        <input type="password" id="new_password" name="new password" />
+        <label htmlFor="confirm_password">Confirm Password</label>
+        <input type="password" id="confirm_password" name="confirm password" />
+        <button>Reset Password</button>
+      </fieldset>
     </div>
   );
 }
