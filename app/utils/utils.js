d730663a128b0f09ad0558cd36c893d2278af258
Evan M. Bonk <embonk@hotmail.com>
2025-09-11 10:16:08 -0600

    added GetMissionByID function

--- app/utils/utils.js
+++ app/utils/utils.js
@@ -105,12 +105,30 @@ export async function GetTaskByStatus(id, token) {
  * @returns returns array
  */
 export async function GetAllMissions(token) {
-  return fetch(`${backend}/missions`, {
+  return fetch(`${backend}/mission`, {
     method: "GET",
     headers: { Authorization: token }
   }).then((res) => res.json());
 };
 
+/**
+ *
+ * @param {integer} id ID of the mission being queried
+ * @param {Token} token context token provided upon login
+ * @returns returns mission name and list of systems
+ */
+export async function GetMissionByID(id, token) {
+  return fetch(`${backend}/mission/${id}`, {
+    method: "GET",
+    headers: { Authorization: token }
+  }).then((res) => res.json())
+}
+
+/**
+ *
+ * @param {Token} token
+ * @returns
+ */
 export async function GetTaskByUser(token) {
   return fetch(`${backend}/user/tasks`, {
     method: "GET",
