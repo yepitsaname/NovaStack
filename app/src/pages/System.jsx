32bc3f0c9f31f5eace3d4552cc67241b57d8fe68
AustinAdams11 <austinadams011@gmail.com>
2025-09-15 11:43:06 -0400

    refactored systems into a component

--- app/src/pages/System.jsx
+++ app/src/pages/System.jsx
@@ -1,25 +1,17 @@
-import { Card, CardContent, Typography, Box, Divider, Table, TableBody, TableCell, TableHead, TableRow, ButtonBase } from "@mui/material";
 import { useState, useEffect, useContext } from "react";
-import { useNavigate } from "react-router"
+
 
 import AppContext from "../AppContext";
 import { Navigate } from "react-router-dom";
+import CapesChart from "../component/CapesChart";
 
 
 export default function System() {
   const [systems, setSystems] = useState([]);
-  const navigate = useNavigate();
   const { token, user, profile } = useContext(AppContext);
 
   if (!token || !user || !profile) return <Navigate to="/login" />
-  const colorMap = {
-    Healthy: "green",
-    Warning: "yellow",
-    Critical: "red",
-    Maintenance: "white",
-    Special_Case: "magenta",
-    Offline: "black"
-  };
+  
 
   useEffect(() => {
     fetch("http://localhost:3000/system/status", {
@@ -31,115 +23,10 @@ export default function System() {
       .catch((err) => console.log(err));
   }, [token]);
 
-  const getStopLight = (status) => {
-    if (status == "Healthy") return "Healthy";
-    if (status == "Warning") return "Warning";
-    if (status == "Critical") return "Critical";
-    if (status == "Maintenance") return "Maintenance";
-    if (status == "Special_Case") return "Special_Case";
-    return "Offline";
-  };
-
-
-  const ColorBlock = ({ status, system }) => (
-    <Box
-      onClick={() => handleClick(system)}
-      sx={{
-        width: 80,
-        height: 80,
-        backgroundColor: colorMap[status],
-        border: "2px solid #333",
-        borderRadius: 2,
-        mx: "auto",
-        cursor: "pointer"
-      }}
-    />
-  );
-
-  const handleClick = (sys) => {
-    if (sys.op_capabilities_available || sys.capabilities_available) {
-      navigate('/reports');
-    } else {
-      alert("Could not find data");
-    }
-  };
-
   return (
     <>
-      <Box sx={{ p: 2 }} display="flex" justifyContent="center" alignItems="center">
-        <Card sx={{ mb: 3, p: 2, backgroundColor: "var(--main-comp-mask)" }}>
-          <CardContent>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell colSpan={systems.length} sx={{ textAlign: 'center' }}>
-                    <Typography variant="h4" align="center" fontWeight={"bold"}>OPSCAP</Typography>
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  {systems.map((sys) => (
-                    <TableCell key={sys.system_id} align="center" sx={{ verticalAlign: "middle" }}>
-                      <Typography variant="h6">StarFall: {sys.system_name}</Typography>
-                    </TableCell>
-                  ))}
-                </TableRow>
-              </TableHead>
-
-              <TableBody>
-                <TableRow>
-                  {systems.map((sys) => (
-                    <TableCell key={sys.system_id + "-opscap"} align="center">
-                      <Box display="flex" justifyContent="center" alignItems="center">
-                        <ColorBlock
-                          status={getStopLight(sys.op_capabilities_available)}
-                          system={sys}
-                        />
-                      </Box>
-                    </TableCell>
-                  ))}
-                </TableRow>
-              </TableBody>
-            </Table>
-          </CardContent>
-        </Card>
-      </Box>
-
-      <Box sx={{ p: 2 }} display="flex" justifyContent="center" alignItems="center">
-        <Card sx={{ mb: 3, p: 2, backgroundColor: "var(--main-comp-mask)" }}>
-          <CardContent>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell colSpan={systems.length} sx={{ textAlign: 'center' }}>
-                    <Typography variant="h4" align="center" fontWeight={"bold"}>SYSCAP</Typography>
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-
-                  {systems.map((sys) => (
-                    <TableCell key={sys.system_id} align="center" sx={{ verticalAlign: "middle" }}>
-                      <Typography variant="h6">StarFall: {sys.system_name}</Typography>
-                    </TableCell>
-                  ))}
-                </TableRow>
-              </TableHead>
-
-              <TableBody>
-                <TableRow>
-                  {systems.map((sys) => (
-                    <TableCell key={sys.system_id + "-syscap"} align="center">
-                      <Box display="flex" justifyContent="center" alignItems="center">
-                        <ColorBlock status={getStopLight(sys.capabilities_available)}
-                          system={sys} />
-                      </Box>
-                    </TableCell>
-                  ))}
-                </TableRow>
-              </TableBody>
-            </Table>
-          </CardContent>
-        </Card>
-      </Box>
+      <CapesChart title="OPSCAP" systems={systems}/>
+      <CapesChart title="SYSCAP" systems={systems}/>
     </>
   );
 }
