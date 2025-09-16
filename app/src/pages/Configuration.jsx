8f577410ab59f30b2ffcdfcc7f0be20d65aa58f3
stako5 <bankhead089@gmail.com>
2025-09-16 17:57:23 +0200

    update syling

--- app/src/pages/Configuration.jsx
+++ app/src/pages/Configuration.jsx
@@ -3,18 +3,6 @@ import AppContext from "../AppContext";
 import { useContext } from "react";
 import { Navigate } from "react-router-dom";
 
-import {
-  Box,
-  FormControl,
-  FormGroup,
-  FormControlLabel,
-  FormLabel,
-  Checkbox,
-  Button,
-  Typography,
-  Divider,
-} from "@mui/material";
-
 const dashboardWidgets = [
   "OPSCAP",
   "SYSCAP",
@@ -53,49 +41,49 @@ function WidgetForm({ label, widgetList, setWidgetList }) {
   };
 
   return (
-<div>
-  <form onSubmit={handleSubmit}>
-    <fieldset >
-      <legend >
-        {label}
-      </legend>
-      <div >
-        {widgetList.map((widget) => (
-          <label key={widget} >
-            <input
-              type="checkbox"
-              checked={selected.includes(widget)}
-              onChange={handleChange}
-              value={widget}
-              style={{ marginRight: '5px' }}
-            />
-            {widget}
-          </label>
-        ))}
-      </div>
-      <div >
-        <button
-          type="submit"
-        >
-          Submit
-        </button>
-        <button
-          type="button"
-          onClick={handleClear}
-        >
-          Clear
-        </button>
-      </div>
-    </fieldset>
-  </form>
-  {submitted.length > 0 && (
     <div >
-      <span>
-        <b>Submitted:</b> {submitted.join(", ")}
-      </span>
+      <form onSubmit={handleSubmit} className="form component">
+        <fieldset >
+          <legend >
+            {label}
+          </legend>
+          <div >
+            {widgetList.map((widget) => (
+              <label key={widget} >
+                <input
+                  type="checkbox"
+                  checked={selected.includes(widget)}
+                  onChange={handleChange}
+                  value={widget}
+                  style={{ marginRight: '5px' }}
+                />
+                {widget}
+              </label>
+            ))}
+          </div>
+          <div >
+            <button
+              type="submit"
+            >
+              Submit
+            </button>
+            <button
+              type="button"
+              onClick={handleClear}
+            >
+              Clear
+            </button>
+          </div>
+        </fieldset>
+      </form>
+      {submitted.length > 0 && (
+        <div >
+          <span>
+            <b>Submitted:</b> {submitted.join(", ")}
+          </span>
+        </div>
+      )}
     </div>
-  )}
-</div>
   );
 }
 export default function Configuration() {
@@ -107,13 +95,10 @@ export default function Configuration() {
   // console.log(dashboardList, systemList, tasksList, reportList);
 
   return (
-<div >
+<div className="dashboard">
   <h4 >
     Configuration
   </h4>
-
-  <hr />
-
   <WidgetForm
     label="Dashboard Config"
     widgetList={dashboardWidgets}
