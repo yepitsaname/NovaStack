dedfff9446df204c25d2f3793d921283377d1165
stako5 <bankhead089@gmail.com>
2025-09-17 18:12:33 +0200

    sfd

--- app/src/pages/Homepage.jsx
+++ app/src/pages/Homepage.jsx
@@ -1,18 +1,9 @@
+import Logo from "../component/Logo";
+
 export default function Homepage() {
   return (
-    <div className="logo-container">
-
-      <svg>
-
-        <polyline className="logo-stroke" points="200,600 200,120 390,330" />
-        <polyline className="logo-stroke" points="372,210 528,78 528,540 270,300 270,420 200,480" />
-        <polyline className="logo-stroke" points="200,600 395,415" />
-
-        <circle className="logo-dot dot1" cx="200" cy="600" r="15" />
-        <circle className="logo-dot dot2" cx="528" cy="78" r="15" />
-        <circle className="logo-dot dot3" cx="200" cy="480" r="15" />
-      </svg>
-
-    </div >
+    <div className="homepage">
+      <Logo/>
+    </div>
   );
 }
