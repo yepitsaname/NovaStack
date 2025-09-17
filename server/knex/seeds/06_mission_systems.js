2632b9cede572c6a9aad64fb0e2d9e5cbf862192
yepitsaname <fernando.g.berry@gmail.com>
2025-09-17 13:19:37 -0400

    updates mission_systems to differentiate opscap and syscap

--- server/knex/seeds/06_mission_systems.js
+++ server/knex/seeds/06_mission_systems.js
@@ -6,15 +6,15 @@ exports.seed = async function (knex) {
   // Deletes ALL existing entries
   await knex('mission_systems').del()
   await knex('mission_systems').insert([
-    { system_name: 'sv 1', status: 1 },
-    { system_name: 'sv 2', status: 1 },
-    { system_name: 'sv 3', status: 1 },
-    { system_name: 'sv bus', status: 1 },
-    { system_name: 'sv payload', status: 1 },
-    { system_name: 'radar 1', status: 1 },
-    { system_name: 'radar 2', status: 1 },
-    { system_name: 'radar 3', status: 1 },
-    { system_name: 'radar watch', status: 1 },
-    { system_name: 'radar ', status: 1 },
+    { system_name: 'sv 1', ops_status: 1, sys_status: 1 },
+    { system_name: 'sv 2', ops_status: 1, sys_status: 1 },
+    { system_name: 'sv 3', ops_status: 1, sys_status: 1 },
+    { system_name: 'sv bus', ops_status: 1, sys_status: 1 },
+    { system_name: 'sv payload', ops_status: 1, sys_status: 1 },
+    { system_name: 'radar 1', ops_status: 1, sys_status: 1 },
+    { system_name: 'radar 2', ops_status: 1, sys_status: 1 },
+    { system_name: 'radar 3', ops_status: 1, sys_status: 1 },
+    { system_name: 'radar watch', ops_status: 1, sys_status: 1 },
+    { system_name: 'radar ', ops_status: 1, sys_status: 1 },
   ]);
 };
