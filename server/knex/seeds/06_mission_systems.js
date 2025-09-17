033f52217d9e39589cd50dd267f1aa327406ebb4
yepitsaname <fernando.g.berry@gmail.com>
2025-09-17 13:01:44 -0400

    updates mission seeds with more data; adds /mission/:id/systems endpoint and verifies it works

--- server/knex/seeds/06_mission_systems.js
+++ server/knex/seeds/06_mission_systems.js
@@ -6,15 +6,15 @@ exports.seed = async function (knex) {
   // Deletes ALL existing entries
   await knex('mission_systems').del()
   await knex('mission_systems').insert([
-    { system_name: 'sv 1', status: 0 },
-    { system_name: 'sv 2', status: 0 },
-    { system_name: 'sv 3', status: 0 },
-    { system_name: 'sv 4', status: 0 },
-    { system_name: 'sv 5', status: 0 },
-    { system_name: 'radar 1', status: 0 },
-    { system_name: 'radar 2', status: 0 },
-    { system_name: 'radar 3', status: 0 },
-    { system_name: 'radar 4', status: 0 },
-    { system_name: 'radar 5', status: 0 },
+    { system_name: 'sv 1', status: 1 },
+    { system_name: 'sv 2', status: 1 },
+    { system_name: 'sv 3', status: 1 },
+    { system_name: 'sv bus', status: 1 },
+    { system_name: 'sv payload', status: 1 },
+    { system_name: 'radar 1', status: 1 },
+    { system_name: 'radar 2', status: 1 },
+    { system_name: 'radar 3', status: 1 },
+    { system_name: 'radar watch', status: 1 },
+    { system_name: 'radar ', status: 1 },
   ]);
 };
