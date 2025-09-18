f00ad823f8173c186798e7fa73c88c0edd85d899
yepitsaname <fernando.g.berry@gmail.com>
2025-09-18 10:55:26 -0400

    updated system status seed file to use abbreviations

--- server/knex/seeds/02_system_status.js
+++ server/knex/seeds/02_system_status.js
@@ -6,11 +6,11 @@ exports.seed = async function (knex) {
   // Deletes ALL existing entries
   await knex("system_status").del();
   await knex("system_status").insert([
-    { sys_status_name: "full mission capable", ops_cap: true, sys_cap: true, color: "green"},
-    { sys_status_name: "partial mission capable", ops_cap: true, sys_cap: true, color: "yellow"},
-    { sys_status_name: "non-mission capable", ops_cap: true, sys_cap: true, color: "red"},
-    { sys_status_name: "asi", ops_cap: true, sys_cap: true, color: "white"},
-    { sys_status_name: "other", ops_cap: true, sys_cap: true, color: "magenta"},
-    { sys_status_name: "offline", ops_cap: true, sys_cap: true, color: "black"}
+    { sys_status_name: "FMC", ops_cap: true, sys_cap: true, color: "green"},
+    { sys_status_name: "PMC", ops_cap: true, sys_cap: true, color: "yellow"},
+    { sys_status_name: "NMC", ops_cap: true, sys_cap: true, color: "red"},
+    { sys_status_name: "ASI", ops_cap: true, sys_cap: true, color: "white"},
+    { sys_status_name: "Other", ops_cap: true, sys_cap: true, color: "magenta"},
+    { sys_status_name: "Offline", ops_cap: true, sys_cap: true, color: "black"}
   ]);
 };
